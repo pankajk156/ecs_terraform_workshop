9a27ab11df61e94b7a1f633aa2d17374677f5365
PANKAJ KUMAR SHARMA <63609112+pankajk156@users.noreply.github.com>
2022-11-17 20:37:54 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,17 +1,37 @@
-module "vpc" {
-  source         = "terraform-aws-modules/vpc/aws"
-  version        = "2.38.0"
-  name           = "test_ecs_provisioning"
-  cidr           = "10.0.0.0/16"
-  azs            = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  public_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+# module "vpc" {
+#   source         = "terraform-aws-modules/vpc/aws"
+ 
+#   name           = "test_ecs_provisioning"
+#   cidr           = "10.0.0.0/16"
+#   azs            = ["us-east-1a", "us-east-1b", "us-east-1c"]
+#   public_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+#   tags = {
+#     "env"       = "dev"
+#   }
+# }
+
+# data "aws_vpc" "main" {
+#   id = module.vpc.vpc_id
+# }
+  
+  module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "test_ecs_provisioning"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
   tags = {
-    "env"       = "dev"
-    "createdBy" = "binpipe"
+    Terraform = "true"
+    Environment = "dev"
   }
-
 }
-
-data "aws_vpc" "main" {
+    data "aws_vpc" "main" {
   id = module.vpc.vpc_id
 }
