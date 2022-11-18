419a80f4c04b1550b5d8af7c4b6ae2fe3d9681e1
pankajk156 <156.pankaj@gmail.com>
2022-11-18 07:48:44 +0530

    s

--- alb.tf
+++ alb.tf
@@ -5,7 +5,7 @@ resource "aws_lb" "test-lb" {
   subnets            = module.vpc.public_subnets
   tags = {
     "env"       = "dev"
-    "createdBy" = "binpipe"
+    
   }
   security_groups = [aws_security_group.lb.id]
 }
